70170bd9c908fc4d9f45144f485cfaa6cb30d0ea
huangmelanie03 <huangmelanie03@gmail.com>
2024-12-11 18:53:46 -0800

    client side routing

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,7 +1,52 @@
-import { Auth, define } from "@calpoly/mustang";
+import { Auth, History, Switch, define } from "@calpoly/mustang";
 import { html, LitElement } from "lit";
 import { HeaderElement } from "./components/header";
 import { HomeViewElement} from "./views/home-view";
+import CafesView from "./views/cafes-view";
+import CafeDetailsView from "./views/cafe-details-view";
+import AboutView from "./views/about-view";
+import LoginView from "./views/login-view";
+import RegisterView from "./views/register-view";
+import ProfileView from "./views/profile-view";
+import WriteReviewView from "./views/write-review-view";
+
+const routes = [
+    {
+        path: "/app/cafes/:id",
+        view: (params:Switch.Params) => html`
+        <cafe-view cafes-id = ${params.id}></cafe-view>`
+    },
+    {
+        path:"/app/cafes",
+        view:() => html`
+        <landing-view></landing-view>`
+    },
+    {
+        path:"/app/about",
+        view: () => html`
+        <about-view></about-view>`
+    },
+    {
+        path:"/app/users/:userId",
+        view: (params: Switch.Params) => html`
+        <profile-view user-id="${params.userId}"></profile-view>`
+    },
+    {
+        path: "/app/login",
+        view: () => html`
+        <login-view></login-view>`
+    },
+    {
+        path: "/app/register",
+        view:() => html`
+        <register-page></register-page>`
+    },
+    {
+        path: "/write-review/:id",
+        view: (params: Switch.Params) => html`
+        <write-review-page cafe-id="${params.id}"></write-review-page>`
+    }
+];
 
 class AppElement extends LitElement {
     static uses = define({
@@ -22,6 +67,19 @@ class AppElement extends LitElement {
 
 define({
     "mu-auth": Auth.Provider,
+    "mu-history": History.Provider,
+    "mu-switch": class AppSwitch extends Switch.Element{
+        constructor(){
+            super(routes, "cafe:history", "cafe:auth");
+        }
+    },
     "cafe-app": AppElement,
-    "cafe-header": HeaderElement
+    "cafe-header": HeaderElement,
+    "cafes-view": CafesView,
+    "cafe-details-view": CafeDetailsView,
+    "about-view": AboutView,
+    "login-view": LoginView,
+    "register-view": RegisterView,
+    "profile-view": ProfileView,
+    "write-review-view": WriteReviewView
 });
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,21 +1,34 @@
 // src/index.ts
-import { connect } from "./services/mongo";
-connect("cafes");
+ connect("Cafes");
 import express, { Request, Response } from "express";
 //import { cafes } from "./models/cafes";
+import { LoginPage } from "./pages/auth";
 import { CafesPage } from "./pages/cafes";
 import { getCafe } from "./services/cafes-svc";
-import cafes from "./routes/cafes";
+import cafes from "./services/cafes-svc"
+import fs from "node:fs/promises";
+import path from "path";
+import auth, { authenticateUser } from "./routes/auth";
 
+import { connect } from "./services/mongo";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const staticDir = process.env.STATIC || "public";
 
-app.use("/api/cafes", cafes);
+connect("cafes");
+
+const staticDir = process.env.STATIC || "public";
 app.use(express.static(staticDir));
 
+// Middleware
 app.use(express.json());
+
+app.use("/app", (req: Request, res: Response) => {
+  const indexHtml = path.resolve(staticDir, "index.html");
+  fs.readFile(indexHtml, { encoding: "utf8" }).then((html) =>
+    res.send(html)
+  );
+});
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
